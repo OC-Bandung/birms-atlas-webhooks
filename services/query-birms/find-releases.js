f1a90e485052adffd84e8e73adfc26bbba4b4236
Mihai Postelnicu <mpostelnicu@gmail.com>
2018-08-17 16:17:39 +0300

    added initial commit of the 3 esssential webhooks, count, find and get-by-ocid

---
+++ services/query-birms/find-releases.js
@@ -0,0 +1,26 @@
+exports = function (payload, response) {
+
+  const mongodb = context.services.get("mongodb-atlas");
+  const releases = mongodb.db("birms").collection("release");
+  var q = payload.query.q || '{}';
+  var fromId = payload.query.fromId || '';
+  var limit = +(payload.query.limit) || 20;
+
+  response.setHeader('content-type', 'application/json');
+
+  var query;
+  if (fromId === '') {
+    query = q;
+  } else {
+    query = '{ \"$and\": [ {\"_id\": {\"$gt\": { \"$oid\": \"' + fromId + '\"}}},' + q + ' ] }';
+  }
+
+  releases.find(EJSON.parse(query)).limit(limit).toArray().then(docs => {
+    docs.map(doc => {
+      doc._id = doc._id.toString();
+    });
+    response.setBody(JSON.stringify(docs));
+  });
+
+
+};
